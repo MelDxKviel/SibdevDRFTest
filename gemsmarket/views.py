08c381b03af00865c86dd7c8d13aeb277be8bcc7
MelDxKviel <mega1hack52@gmail.com>
2022-02-10 19:45:45 +0700

    ADD: customer deals view

--- gemsmarket/views.py
+++ gemsmarket/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import views
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Customer
+from .serializers import CustomerDealsSerializer, DealsFileSerializer
+from .services import import_deals
+
+
+class CustomerDealsAPIView(views.APIView):
+    serializer_class = DealsFileSerializer
+
+    def get(self, request, *args, **kwargs):
+        queryset = Customer.objects.all()[:5]
+        serializer_class = CustomerDealsSerializer
+        response = {'response': serializer_class(queryset.all(), many=True).data}
+        return Response(response, status=200)
+
+    def post(self, request, *args, **kwargs):
+        serializer = DealsFileSerializer(data=request.data)
+        if serializer.is_valid():
+            file = serializer.validated_data['file']
+            data = import_deals(file)
+            return Response(data=data, status=200)
+        else:
+            data = {'Status': 'Error', 'Desc': serializer.errors}
