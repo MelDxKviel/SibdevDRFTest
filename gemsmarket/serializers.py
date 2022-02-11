46f24b0c4dfc943040692d34941f37bd301fd849
MelDxKviel <mega1hack52@gmail.com>
2022-02-11 23:31:21 +0700

    ADD: gems field in customer serializer

--- gemsmarket/serializers.py
+++ gemsmarket/serializers.py
@@ -3,9 +3,11 @@ from .models import Customer
 
 
 class CustomerDealsSerializer(serializers.ModelSerializer):
+    gems = serializers.StringRelatedField(source='load_gems_list', many=True)
+
     class Meta:
         model = Customer
-        fields = ('username', 'spent_money')
+        fields = ('username', 'spent_money', 'gems')
 
 
 class DealsFileSerializer(serializers.Serializer):
