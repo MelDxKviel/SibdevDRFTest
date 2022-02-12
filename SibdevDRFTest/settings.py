b8b2ad4048fe03194456aa1017d1a7f6bff1f5b7
MelDxKviel <mega1hack52@gmail.com>
2022-02-12 18:29:53 +0700

    ADD: 0.0.0.0 to allowed hosts

--- SibdevDRFTest/settings.py
+++ SibdevDRFTest/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%!1qvg*uy285p_$ye37#(zi_(1oo#-bx%mx6aob#rnt4ygjwtu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
